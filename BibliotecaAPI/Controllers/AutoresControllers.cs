1ed9831e23ba57123eac9e68767a67a4f4d7b1fe
Angel Maya <jangelmaya7@gmail.com>
2025-09-18 22:57:22 -0600

    Agregar .gitignore y limpiar archivos temporales

---
+++ BibliotecaAPI/Controllers/AutoresControllers.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using BibliotecaAPI.Entidades;
+using BibliotecaAPI.Datos;
+using System.Reflection.Metadata.Ecma335;
+using Microsoft.EntityFrameworkCore;
+
+namespace BibliotecaAPI.Controllers
+{
+
+    [ApiController]
+    [Route("api/autores")] //  ruta donde es enviada la peticion http 
+    public class AutoresControllers : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public AutoresControllers(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Autor>> Get()
+        {
+            {
+                return await context.Autores.ToListAsync();
+            }; 
+        }
+
+        [HttpGet("{id:int}")] // api/autores/id
+        public async Task<ActionResult<Autor>> Get(int id)
+            {
+            var autor = await context.Autores
+                .Include(x => x.Libros)
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            if (autor is null)
+            {
+                return NotFound();
+            }
+
+            return autor;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Autor autor)
+        {
+            context.Add(autor);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpPut("{id:int}")] // api/autores/id
+        public async Task<ActionResult> Put(int id, Autor autor)
+        {
+            if (id != autor.Id)
+            {
+                return BadRequest("Los ids deben de coincidir");
+            }
+
+            context.Update(autor);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var registroBorrados = await context.Autores.Where(x => x.Id == id).ExecuteDeleteAsync();
+
+            if (registroBorrados == 0)
+            {
+                return NotFound();
+            }
+            return Ok();
+        }
+        
+    }
+}
---
+++ BibliotecaAPI/Datos/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BibliotecaAPI.Entidades;
+
+namespace BibliotecaAPI.Datos
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Autor> Autores { get; set; }
+        public DbSet<Libro> Libros { get; set; }
+    }
+}
